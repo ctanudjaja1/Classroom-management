cf98b853dbbda6a87c0b9dc4e82c536d683bd2d9
Ctanudjaja1 <Christan8@yahoo.com>
2026-01-29 21:46:19 -0500

    added Dashboard and Subjects to UI

---
+++ classroom-frontend/src/pages/subjects/list.tsx
@@ -0,0 +1,11 @@
+import {ListView} from "@/components/refine-ui/views/list-view.tsx";
+import {Breadcrumb} from "@/components/ui/breadcrumb.tsx";
+
+const SubjectsList = () => {
+    return (
+        <ListView>
+            <Breadcrumb />
+        </ListView>
+    )
+}
+export default SubjectsList

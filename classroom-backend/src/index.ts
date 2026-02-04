8efeed936eff85236caab79422568b85374f8fa6
Ctanudjaja1 <Christan8@yahoo.com>
2026-02-04 14:38:46 -0500

    implement safe SQL pattern

--- classroom-backend/src/index.ts
+++ classroom-backend/src/index.ts
@@ -5,6 +5,10 @@ import subjectsRouter from "./routes/subjects"
 const app = express();
 const PORT = 8000;
 
+if(!process.env.FRONTEND_URL){
+    throw new Error('FRONTEND_URL is not set in the environment');
+}
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'PUT','POST', 'DELETE'],

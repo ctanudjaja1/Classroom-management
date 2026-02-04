a1887390acd11306baa76e99e4daddd069fb3224
Ctanudjaja1 <Christan8@yahoo.com>
2026-02-04 15:21:25 -0500

    implement rest data provider

--- classroom-frontend/src/index.tsx
+++ classroom-frontend/src/index.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "@/dev";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-            <App/>
-        </DevSupport>
+        <App />
     </React.StrictMode>
 );
